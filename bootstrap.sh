0474a8e916b634dd0a70cafefbaccfe50703e5e0
chrono <chrono@open-resource.org>
2016-12-31 17:33:03 +0000

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+./scripts/feeds update -a
+./scripts/feeds install qmp-big-node tmux synctincvpn tinc reghack b6m b6m-spread lunced nc bmx6-mdns gwck-qmp htop macchanger bmx6 bmx6-json bmx6-sms bmx6-table bmx6-uci-config luci-app-bmx6 luci-lib-libremap-bmx6
